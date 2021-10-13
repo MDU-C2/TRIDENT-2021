f5c87dedfc43a359706cb0ccaeddd6772c5efd6a
Valentin Johansson <valentinjohan@outlook.com>
2021-10-13 17:08:12 +0200

    Add service class and dependencies

--- trident_software/baseclasses/navigationbase.py
+++ trident_software/baseclasses/navigationbase.py
@@ -1,2 +1,17 @@
 import rclpy
-from rclpy import Node
+from rclpy.node import Node
+
+from LoadMissionPlan.srv import LoadMissionPlan
+
+class NavigationBase(Node):
+
+    def __init__(self, node_name):
+        super().__init__(node_name)
+        self.srv = self.create_service(LoadMissionPlan, 'nav/wp_queue/', self.load_mission_plan_callback)
+
+    def load_mission_plan_callback(self, request, response):
+        for latitude,longitude in request.latitude,request.longitude:
+            self.get_logger().info('Incoming request\na: %d b: %d' % (latitude, longitude))
+
+        response = 1
+        return response
