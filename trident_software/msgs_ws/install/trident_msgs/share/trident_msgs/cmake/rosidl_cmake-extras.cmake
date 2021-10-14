4f5bd10c6d9dd14f97de2b0b4e916d9dc11d930d
johannes <jdeivard@outlook.com>
2021-10-14 16:21:44 +0200

    Added some message definitions and configured CMakeList file

---
+++ trident_software/msgs_ws/install/trident_msgs/share/trident_msgs/cmake/rosidl_cmake-extras.cmake
@@ -0,0 +1,4 @@
+# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
+
+set(trident_msgs_IDL_FILES "msg/Waypoint.idl;msg/WaypointAction.idl;msg/Mission.idl")
+set(trident_msgs_INTERFACE_FILES "msg/Waypoint.msg;msg/WaypointAction.msg;msg/Mission.msg")
