7f839fd554656b3c814ff3f2343357e3aac0a763
johannes <jdeivard@outlook.com>
2021-10-14 16:56:04 +0200

    Added LoadMission.srv

--- trident_software/msgs_ws/install/trident_msgs/share/trident_msgs/cmake/rosidl_cmake-extras.cmake
+++ trident_software/msgs_ws/install/trident_msgs/share/trident_msgs/cmake/rosidl_cmake-extras.cmake
@@ -1,4 +1,4 @@
 # generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
 
-set(trident_msgs_IDL_FILES "msg/Waypoint.idl;msg/WaypointAction.idl;msg/Mission.idl")
-set(trident_msgs_INTERFACE_FILES "msg/Waypoint.msg;msg/WaypointAction.msg;msg/Mission.msg")
+set(trident_msgs_IDL_FILES "msg/Waypoint.idl;msg/WaypointAction.idl;msg/Mission.idl;srv/LoadMission.idl")
+set(trident_msgs_INTERFACE_FILES "msg/Waypoint.msg;msg/WaypointAction.msg;msg/Mission.msg;srv/LoadMission.srv;srv/LoadMission_Request.msg;srv/LoadMission_Response.msg")
