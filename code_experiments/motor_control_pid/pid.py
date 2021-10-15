2919e88a89b96ac6d963178d84c44205262249ea
Valentin Johansson <valentinjohan@outlook.com>
2021-10-15 17:01:43 +0200

    Migrate old pid class function to python

--- code_experiments/motor_control_pid/pid.py
+++ code_experiments/motor_control_pid/pid.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 #///////////////////////////////
 #       PID CLASS
@@ -173,3 +174,148 @@ class PID():
       self.currentGyroY = gyroY
       self.currentGyroZ = gyroZ
       self.currentPosZ = depth
+
+    def Trapezoid_Integration(self):
+      self.currentXVelocity = self.currentXVelocity + self.deltaTime*((self.currentAccX + self.lastAccX)/2.0)
+      self.currentYVelocity = self.currentYVelocity + self.deltaTime*((self.currentAccY + self.lastAccY)/2.0)
+
+
+    def Rectangular_Integration(self):
+      self.currentXVelocity = self.currentXVelocity + (self.currentAccX) * self.deltaTime
+      self.currentYVelocity = self.currentYVelocity + (self.currentAccY) * self.deltaTime
+
+
+    def Anti_Spike_Filter(self):
+      spikeLimit = 0.05 #find correct value
+      if abs(self.currentAccX) < spikeLimit:
+         currentAccX = 0.0
+
+      if abs(self.currentAccY) < spikeLimit:
+         self.currentAccY = 0.0
+
+      if self.currentAccX != 0.0 and self.lastAccX == 0.0:
+         self.lastAccX = self.currentAccX
+         self.currentAccX = 0.0
+      else:
+         self.lastAccX = self.currentAccX
+
+      if self.currentAccY != 0.0 and self.lastAccY == 0.0:
+         self.lastAccY = self.currentAccY
+         self.currentAccY = 0.0
+      else:
+         self.lastAccY = self.currentAccY
+      self.Rectangular_Integration()
+
+    def Update_Thruster_Value(self, motorNumber):
+      # thruster =   - - - - - - x-axis
+      #              - - - - - - y-axis
+      #              - - - - - - z-axis
+      #              - - - - - - x rotation
+      #              - - - - - - y rotation
+      #              - - - - - - z rotation
+      self.thruster[motorNumber] = 128.0 + ((self.posX_P_Value  * self.xPIDconfig(1,1) + self.posX_I_Value  * self.xPIDconfig(2,1)  +self.posX_D_Value  * self.xPIDconfig(3,1) )    *self.xThrusterConfig(motorNumber,1)) + \
+                                           ((self.posY_P_Value  * self.xPIDconfig(1,2) + self.posY_I_Value  * self.xPIDconfig(2,2)  +self.posY_D_Value  * self.xPIDconfig(3,2) )    *self.xThrusterConfig(motorNumber,2)) + \
+                                           ((self.posZ_P_Value  * self.xPIDconfig(1,3) + self.posZ_I_Value  * self.xPIDconfig(2,3)  +self.posZ_D_Value  * self.xPIDconfig(3,3) )    *self.xThrusterConfig(motorNumber,3)) + \
+                                           ((self.Roll_P_Value  * self.xPIDconfig(1,4) + self.Roll_I_Value  * self.xPIDconfig(2,4)  +self.Roll_D_Value  * self.xPIDconfig(3,4) )    *self.xThrusterConfig(motorNumber,4)) + \
+                                           ((self.Pitch_P_Value * self.xPIDconfig(1,5) + self.Pitch_I_Value * self.xPIDconfig(2,5)  +self.Pitch_D_Value * self.xPIDconfig(3,5) )    *self.xThrusterConfig(motorNumber,5)) + \
+                                           ((self.Yaw_P_Value   * self.xPIDconfig(1,6) + self.Yaw_I_Value   * self.xPIDconfig(2,6)  +self.Yaw_D_Value   * self.xPIDconfig(3,6) )    *self.xThrusterConfig(motorNumber,6))  
+
+      if self.thruster[self.motorNumber] < 0.0:
+         self.thruster[self.motorNumber] = 0.0
+      elif self.thruster[self.motorNumber] > 255.0:
+         self.thruster[self.motorNumber] = 255.0
+
+    '''
+   procedure Update_Errors is
+   begin
+
+      errorYaw   := currentYaw - desiredYaw;
+      if abs(errorYaw) < zeroLimit then
+         errorYaw := 0.0;
+      end if;
+
+      errorPitch := currentPitch - desiredPitch;
+      if abs(errorPitch) < zeroLimit then
+         errorPitch := 0.0;
+      end if;
+
+      errorRoll  := currentRoll  - desiredRoll;
+      if abs(errorRoll) < zeroLimit then
+         errorRoll := 0.0;
+      end if;
+
+      errorPosX  := currentPosX - desiredPosX;
+      errorPosY  := currentPosY - desiredPosY;
+      errorPosZ  := currentPosZ - desiredPosZ;
+
+   end Update_Errors;
+
+
+
+   procedure Update_Last_Errors is
+
+   begin
+
+      --Update last errors
+      lastErrorYaw   := errorYaw;
+      lastErrorPitch := errorPitch;
+      lastErrorRoll  := errorRoll;
+
+      lastErrorPosX  := errorPosX;
+      lastErrorPosY  := errorPosY;
+      lastErrorPosZ  := errorPosZ;
+   end Update_Last_Errors;
+
+   procedure Update_PID_orientation is
+   begin
+      --P for all ORIENTATION
+      Pitch_P_Value := errorPitch;
+      roll_P_Value  := errorRoll;
+      yaw_P_Value   := errorYaw;
+
+      --I for all ORIENTATION
+      Pitch_I_Value := pitch_I_Value  + errorPitch*deltaTime;
+      roll_I_Value  := roll_I_Value   + errorRoll *deltaTime;
+      yaw_I_Value   := yaw_I_Value    + erroryaw  *deltaTime;
+
+      --D for all ORIENTATION
+      Pitch_D_Value := (errorPitch-lastErrorPitch)/deltaTime;
+      roll_D_Value  := (errorRoll-lastErrorRoll)  /deltaTime;
+      yaw_D_Value   := (errorYaw-lastErrorYaw)    /deltaTime;
+   end Update_PID_orientation;
+
+
+   procedure Update_PID_position is
+
+   begin
+      --P for all POSITIONS
+      posX_P_Value  :=  errorPosX;
+      posY_P_Value  :=  errorPosY;
+      posZ_P_Value  :=  errorPosZ;
+
+      --I for all POSITIONS
+      posX_I_Value  := posX_I_Value + errorPosX*deltaTime;
+      posY_I_Value  := posY_I_Value + errorPosY*deltaTime;
+      posZ_I_Value  := posZ_I_Value + errorPosZ*deltaTime;
+      --D for all POSITIONS
+
+      posX_D_Value  := (errorPosX-lastErrorPosX)/deltaTime;
+      posY_D_Value  := (errorPosY-lastErrorPosY)/deltaTime;
+      posZ_D_Value  := (errorPosZ-lastErrorPosZ)/deltaTime;
+
+   end Update_PID_position;
+
+
+   procedure Go_To_Desired_Position_And_Orientation is
+   begin
+      --add positionx/y integration if using position.
+      Update_Errors;
+      Update_PID_position;
+      Update_PID_orientation;
+      Update_Last_Errors;
+      for i in 1..6 loop
+         update_thruster_value(i);
+      end loop;
+
+   end Go_To_Desired_Position_And_Orientation;
+    '''
