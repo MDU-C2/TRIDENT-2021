838e900138eaf07b17f94778d35b3c86619f5bb8
Valentin Johansson <valentinjohan@outlook.com>
2021-10-18 10:54:13 +0200

    Migrate remaining functions

--- code_experiments/motor_control_pid/main.py
+++ code_experiments/motor_control_pid/main.py
@@ -1,8 +1,32 @@
 from pid import PID
 
+pid = PID()
+
+def getSensorValues():
+    pid.currentRoll     = 0
+    pid.currentPitch    = 0
+    pid.currentYaw      = 0
+    pid.currentPosX     = 0
+    pid.currentPosY     = 0
+    pid.currentPosZ     = 0
+
+def setDesiredState():
+    relativeState = False
+    x = 0
+    y = 0
+    z = 0
+    roll = 0
+    pitch = 0
+    yaw = 0
+
+    if (relativeState):
+        pid.setDesiredRelativeState(x,y,z,roll,pitch,yaw)
+    else:
+        pid.setDesiredState(x,y,z,roll,pitch,yaw)
+
+
 def main():
-    pid = PID()
-    print(pid.xThrusterConfig)
+    print("starting...")
 
 
 if __name__ == '__main__':
