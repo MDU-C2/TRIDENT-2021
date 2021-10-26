36d40080b882ed172b79ec337e098da8e193c600
Valentin Johansson <valentinjohan@outlook.com>
2021-10-26 17:15:37 +0200

    WIP on adding interfaces, issues with sending simultaneously
    Sending simultaneously to athena and naiad seem to crash the server :(.

--- trident_software/ground_control/gui_backend_ws/tester.js
+++ trident_software/ground_control/gui_backend_ws/tester.js
@@ -5,26 +5,73 @@ const rclnodejs = require('rclnodejs');
 rclnodejs
   .init()
   .then(() => {
-    const node = rclnodejs.createNode('subscription_message_example_node');
+    const node = rclnodejs.createNode('tester_node');
     let count = 0;
 
-    node.createSubscription(
-      'trident_msgs/msg/Num',
-      'topic2',
-      (state) => {
-        console.log(`Received message No. ${++count}: `, state);
+    //hearbeat tester
+    const heartbeatPublisherAthena = node.createPublisher('trident_msgs/msg/Num','hearbeat/athena');
+    let counter1 = 0;
+    setInterval(() => {
+      heartbeatPublisherAthena.publish({a:counter1++});
+    }, 500);
+    
+    const heartbeatPublisherNaiad = node.createPublisher('trident_msgs/msg/Num','hearbeat/naiad');
+    let counter2 = 0;
+    setInterval(() => {
+      heartbeatPublisherNaiad.publish({a:counter2++});
+    }, 500);
+
+
+    //toggle contrl mode on athena
+    node.createService(
+      'trident_msgs/srv/ToggleControl',
+      'toggle_control_mode',
+      (request, response) => {
+        console.log(request);
+        console.log('Request to toggle on');
+        let result = response.template;
+        result.success = true;
+        console.log(`Sending response: ${typeof result}`, result, '\n--');
+        response.send(result);
       }
     );
 
-    const publisher = node.createPublisher('trident_msgs/msg/Num','hearbeat/athena');
+    //abort tester
+    node.createService(
+      'trident_msgs/srv/Abort',
+      'abort',
+      (request, response) => {
+        console.log(request);
+        console.log('Request to toggle on');
+        let result = response.template;
+        result.success = true;
+        console.log(`Sending response: ${typeof result}`, result, '\n--');
+        response.send(result);
+      }
+    );
 
-    let counter = 0;
-    setInterval(() => {
-      publisher.publish({a:counter++});
-    }, 500);
+    //toggle mode on naiad
+    /*
+    node.createService(
+      'trident_msgs/srv/ToggleControl',
+      'toggle_control_mode',
+      (request, response) => {
+        console.log('Request to toggle automatic on Athena');
+        let result = response.template;
+        result.success = true;
+        result.message = "automatic toggled";
+        console.log(`Sending response: ${typeof result}`, result, '\n--');
+        response.send(result);
+      }
+    );*/
 
     rclnodejs.spin(node);
   })
   .catch((e) => {
     console.log(e);
+  });
+
+  process.on('SIGINT', function() {
+    console.log('Shutting down server.');
+    process.exit();
   });
