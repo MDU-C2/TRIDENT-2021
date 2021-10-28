38fa32d200308bd478253f5699576cef6600b4f2
Valentin Johansson <valentinjohan@outlook.com>
2021-10-28 17:01:04 +0200

    Add mission interface, add [lat,lng]->[x,y] conversion

--- trident_software/ground_control/gui_backend_ws/tester.js
+++ trident_software/ground_control/gui_backend_ws/tester.js
@@ -1,6 +1,77 @@
 
-const rclnodejs = require('rclnodejs');
+const rclnodejs = require('./node_modules/rclnodejs/index.js')
+const Mission = rclnodejs.require('trident_msgs/msg/Mission');
+const LoadMission = rclnodejs.require('trident_msgs/srv/LoadMission');
+const Waypoint = rclnodejs.require('trident_msgs/msg/Waypoint');
+const WaypointAction = rclnodejs.require('trident_msgs/msg/WaypointAction');
+const Pose = rclnodejs.require('geometry_msgs/msg/Pose');
 
+class StartMissionActionServer {
+  constructor(node) {
+    this._node = node;
+
+    this._actionServer = new rclnodejs.ActionServer(
+      node,
+      'trident_msgs/msg/Mission',
+      'startMission',
+      this.executeCallback.bind(this),
+      this.goalCallback.bind(this),
+      null,
+      this.cancelCallback.bind(this)
+    );
+  }
+
+  async executeCallback(missionHandle) {
+    this._node.getLogger().info('Executing goal...');
+
+    const feedbackMessage = new Fibonacci.Feedback();
+    const sequence = [0, 1];
+
+    // Start executing the action
+    for (let i = 1; i < missionHandle.request.order; i++) {
+      // Check if the goal has been canceled
+      if (missionHandle.isCancelRequested) {
+        missionHandle.canceled();
+        this._node.getLogger().info('Goal canceled');
+        return new Fibonacci.Result();
+      }
+
+      // Update Fibonacci sequence
+      sequence.push(sequence[i] + sequence[i - 1]);
+
+      feedbackMessage.sequence = sequence;
+      this._node
+        .getLogger()
+        .info(`Publishing feedback: ${feedbackMessage.sequence}`);
+
+      // Publish the feedback
+      missionHandle.publishFeedback(feedbackMessage);
+
+      // Wait for 1 second
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+
+    missionHandle.succeed();
+
+    // Populate result message
+    const result = new Fibonacci.Result();
+    result.sequence = sequence;
+
+    this._node.getLogger().info(`Returning result: ${result.sequence}`);
+
+    return result;
+  }
+
+  goalCallback(missionHandle) {
+    this._node.getLogger().info('Received goal request');
+    return rclnodejs.GoalResponse.ACCEPT;
+  }
+
+  cancelCallback(missionHandle) {
+    this._node.getLogger().info('Received cancel request');
+    return rclnodejs.CancelResponse.ACCEPT;
+  }
+}
 
 rclnodejs
   .init()
@@ -38,11 +109,19 @@ rclnodejs
 
     //load mission plan tester (athena)
     node.createService(
-      'std_srvs/srv/Trigger',
+      'trident_msgs/srv/LoadMission',
       'load_mission_plan/athena',
       (request, response) => {
-        console.log(request);
-        console.log('Request to load mission plan on athena');
+        let loadMission = new LoadMission();
+        let mission = new Mission();
+        let waypoint = new Waypoint();
+        let wpAction = new WaypointAction();
+        let pose = new Pose();
+        mission = request.mission;
+        waypoint = mission.waypoints;
+        console.log(waypoint);
+        //console.log(JSON.stringify(request));
+        //console.log('Request to load '+JSON.stringify(request)+' on athena');
         let result = response.template;
         result.success = true;
         result.message = "mission plan loaded on athena";
@@ -56,8 +135,7 @@ rclnodejs
       'std_srvs/srv/Trigger',
       'load_mission_plan/naiad',
       (request, response) => {
-        console.log(request);
-        console.log('Request to load mission plan on naiad');
+        //console.log('Request to load mission plan on naiad');
         let result = response.template;
         result.success = true;
         result.message = "mission plan loaded on naiad";
@@ -65,6 +143,9 @@ rclnodejs
         response.send(result);
       }
     );
+
+    //start mission plan tester
+    //new StartMissionActionServer(node);
 
     //abort tester
     node.createService(
