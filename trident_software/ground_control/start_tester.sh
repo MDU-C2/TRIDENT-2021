c97c36a3268e7e3f9f649d7a0c5d89f9b2b4918a
Valentin Johansson <valentinjohan@outlook.com>
2021-11-02 16:02:00 +0100

    Add get state services, add athena&naiad markers, add rotation plugin

--- trident_software/ground_control/start_tester.sh
+++ trident_software/ground_control/start_tester.sh
@@ -5,11 +5,10 @@ source ~/ros2_foxy/install/setup.bash
 
 #Go to root of trident software
 cd ~/Documents/gitclones/TRIDENT-2021/trident_software/
-#Build project
-colcon build
+
 #Source project
 . install/setup.bash
 
 #Start ground control server
 cd ground_control/gui_backend_ws/
-node index.js
+node tester.js
