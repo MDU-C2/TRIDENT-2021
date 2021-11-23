b6ee792aed29d97ddc5662bb2783377c3208eb6a
Felix Sjoqvist <fst17001@student.mdh.se>
2021-11-23 10:04:34 +0000

    fixed better template for ros node

--- trident_software/vision_ws/src/openvslam/src/optflow/run_optflow.cc
+++ trident_software/vision_ws/src/openvslam/src/optflow/run_optflow.cc
@@ -2,6 +2,7 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <thread>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
@@ -15,19 +16,18 @@ class OpticalFlowNode : public rclcpp::Node
             : Node("optical_flow_node"), count_(0)
         {
             publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-            timer_ = this->create_wall_timer(
-                    500ms, std::bind(&OpticalFlowNode::timer_callback, this));
-            /* Start optical flow algorithm here, maybe in its own thread. */
+            while(true){ /* Start optical flow algorithm here, maybe in its own thread. */
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+                auto message = std_msgs::msg::String();
+                message.data = "Hello, world! " + std::to_string(count_++);
+                RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+                publisher_->publish(message);
+                if(count_ > 10)
+                    break;
+            }
         }
 
     private:
-        void timer_callback()
-        {
-            auto message = std_msgs::msg::String();
-            message.data = "Hello, world! " + std::to_string(count_++);
-            RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-            publisher_->publish(message);
-        }
         rclcpp::TimerBase::SharedPtr timer_;
         rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
         size_t count_;
@@ -36,7 +36,7 @@ class OpticalFlowNode : public rclcpp::Node
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<OpticalFlowNode>());
+    rclcpp::spin_some(std::make_shared<OpticalFlowNode>());
     rclcpp::shutdown();
     return 0;
 }
