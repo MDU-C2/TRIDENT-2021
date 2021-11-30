9e15b3a874bf7e7bb6f4fd046368a3d2da34f1da
Valentin Johansson <valentinjohan@outlook.com>
2021-11-30 14:20:06 +0100

    Update unit test startup script
    Add test cases, test summary, and logging

--- trident_software/testing/unit_tests/start_all.sh
+++ trident_software/testing/unit_tests/start_all.sh
@@ -25,43 +25,150 @@ printf "${cyn}\n=========================\nStart unit tests\n===================
 cd testing/unit_tests
 
 #Athena test cases
-printf "${cyn}\n--- Athena mission control ---\n${end}"
-launch_test athena/athena_mission_control_test.launch.py
+printf "${cyn}\nStart Athena mission control test...\n${end}"
+launch_test athena/athena_mission_control_test.launch.py &> logs.txt
+if [ $? -ne 0 ]
+then
+    res_athena_mission_control="${red}Fail${end}"
+else
+    res_athena_mission_control="${grn}Pass${end}"
+fi
 
-printf "${cyn}\n--- Athena navigation ---\n${end}"
-launch_test athena/athena_navigation_test.launch.py
+printf "${cyn}\nStart Athena navigation test...\n${end}"
+launch_test athena/athena_navigation_test.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_athena_navigation="${red}Fail${end}"
+else
+    res_athena_navigation="${grn}Pass${end}"
+fi
 
-printf "${cyn}\n--- Athena motor control ---\n${end}"
-launch_test athena/athena_hold_pose.launch.py
-launch_test athena/athena_go_to_pose.launch.py
-launch_test athena/athena_manual_override.launch.py
+printf "${cyn}\nStart Athena motor control test...\n${end}"
+res_athena_motor_control="${grn}Pass${end}"
 
-printf "${cyn}\n--- Athena motor driver ---\n${end}"
-launch_test athena/athena_get_state.launch.py
-launch_test athena/athena_activate_motors.launch.py
-launch_test athena/athena_kill_motors.launch.py
-launch_test athena/athena_motor_outputs.launch.py
+launch_test athena/athena_hold_pose.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_athena_motor_control="${red}Fail${end}" 
+fi
+launch_test athena/athena_go_to_pose.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_athena_motor_control="${red}Fail${end}" 
+fi
+launch_test athena/athena_manual_override.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_athena_motor_control="${red}Fail${end}" 
+fi
 
-#printf "\n--- Athena position ---\n${end}"
+printf "${cyn}\nStart Athena motor driver test...\n${end}"
+res_athena_motor_driver="${grn}Pass${end}"
+launch_test athena/athena_get_state.launch.py &>> logs.txt
+
+launch_test athena/athena_activate_motors.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_athena_motor_driver="${red}Fail${end}"
+fi
+launch_test athena/athena_kill_motors.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_athena_motor_driver="${red}Fail${end}"
+fi
+launch_test athena/athena_motor_outputs.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_athena_motor_driver="${red}Fail${end}"
+fi
+
+#printf "\nStart Athena position test...\n${end}"
 #launch_test naiad/naiad_navigation_test.launch.py
 
-printf "${cyn}\n--- Athena guidance ---\n${end}"
-launch_test athena/athena_guidance_system_test.launch.py
+printf "${cyn}\nStart Athena guidance system test...\n${end}"
+launch_test athena/athena_guidance_system_test.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_athena_guidance_system="${red}Fail${end}"
+else
+    res_athena_guidance_system="${grn}Pass${end}"
+fi
 
 #Naiad test cases
-printf "${cyn}\n--- Naiad mission control ---\n${end}"
-launch_test naiad/naiad_mission_control_test.launch.py
+printf "${cyn}\nStart Naiad mission control test...\n${end}"
+launch_test naiad/naiad_mission_control_test.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_naiad_mission_control="${red}Fail${end}"
+else
+    res_naiad_mission_control="${grn}Pass${end}"
+fi
 
-printf "${cyn}\n--- Naiad navigation ---\n${end}"
-launch_test naiad/naiad_navigation_test.launch.py
+printf "${cyn}\nStart Naiad navigation test...\n${end}"
+launch_test naiad/naiad_navigation_test.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_naiad_navigation="${red}Fail${end}"
+else
+    res_naiad_navigation="${grn}Pass${end}"
+fi
 
-printf "${cyn}\n--- Naiad motor control ---\n${end}"
-launch_test naiad/naiad_hold_pose.launch.py
-launch_test naiad/naiad_go_to_pose.launch.py
-launch_test naiad/naiad_manual_override.launch.py
+printf "${cyn}\nStart Naiad motor control test...\n${end}"
+res_naiad_motor_control="${grn}Pass${end}"
 
-printf "${cyn}\n--- Naiad motor driver ---\n${end}"
-launch_test naiad/naiad_get_state.launch.py
-launch_test naiad/naiad_activate_motors.launch.py
-launch_test naiad/naiad_kill_motors.launch.py
-launch_test naiad/naiad_motor_outputs.launch.py
+launch_test naiad/naiad_hold_pose.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_naiad_motor_control="${red}Fail${end}"  
+fi
+launch_test naiad/naiad_go_to_pose.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_naiad_motor_control="${red}Fail${end}"  
+fi
+launch_test naiad/naiad_manual_override.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_naiad_motor_control="${red}Fail${end}"  
+fi
+
+printf "${cyn}\nStart Naiad motor driver test...\n${end}"
+launch_test naiad/naiad_get_state.launch.py &>> logs.txt
+res_naiad_motor_driver="${grn}Pass${end}"
+
+launch_test naiad/naiad_activate_motors.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_naiad_motor_driver="${red}Fail${end}"
+fi
+launch_test naiad/naiad_kill_motors.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_naiad_motor_driver="${red}Fail${end}"
+fi
+launch_test naiad/naiad_motor_outputs.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_naiad_motor_driver="${red}Fail${end}"
+fi
+
+
+#Print test results
+printf "\n+---------------------------------------+-----------+\n"
+printf "| Test results: Athena                  |   Status  |\n"
+printf "+---------------------------------------+-----------+\n"
+printf "| Mission control                       |   %s    |\n" $res_athena_mission_control
+printf "| Navigation                            |   %s    |\n" $res_athena_navigation
+printf "| Motor control                         |   %s    |\n" $res_athena_motor_control
+printf "| Motor driver                          |   %s    |\n" $res_athena_motor_driver
+printf "| Guidance system                       |   %s    |\n" $res_athena_guidance_system
+printf "+---------------------------------------+-----------+\n\n"
+
+printf "+---------------------------------------+-----------+\n"
+printf "| Test results: Naiad                   |   Status  |\n"
+printf "+---------------------------------------+-----------+\n"
+printf "| Mission control                       |   %s    |\n" $res_naiad_mission_control
+printf "| Navigation                            |   %s    |\n" $res_naiad_navigation
+printf "| Motor control                         |   %s    |\n" $res_naiad_motor_control
+printf "| Motor driver                          |   %s    |\n" $res_naiad_motor_driver
+printf "+---------------------------------------+-----------+\n\n"
