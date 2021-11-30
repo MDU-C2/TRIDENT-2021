74e1bd0fa97ee1563844c58ab0afab3435e43c69
Johannes Deivard <jdd14001@student.mdh.se>
2021-11-30 15:00:52 +0100

    PWM wip

--- trident_software/athena_ws/src/athena_driver/athena_driver/motordrivermain.py
+++ trident_software/athena_ws/src/athena_driver/athena_driver/motordrivermain.py
@@ -78,7 +78,7 @@ class MotorDriverNode(MotorDriverBase):
         try:
 
             self.get_logger().info(f"motor_id: {str(motor_id)}")
-            pwm = self._pwm_containers[motor_id]["pwm"]
+            pwm = self._pwm_containers[str(motor_id)]["pwm"]
             pw = self.get_pulse_width(power)
             self.get_logger().info(f"PW: {pw}")
             dc = self.get_duty_cycle(pw)
