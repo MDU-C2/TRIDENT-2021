4306821def1c3df7a09dba6aaa2e4cc254412ef2
Johannes <jdeivard@outlook.com>
2021-12-07 18:49:24 +0100

    Switched to another watchdog timer approach using timestamps instead of relying on cancels and resets

--- trident_software/baseclasses/motordriverbase.py
+++ trident_software/baseclasses/motordriverbase.py
@@ -9,6 +9,7 @@ from rclpy.node import Node
 from rclpy.action import ActionClient, ActionServer
 from std_srvs.srv import Trigger
 import json
+import numpy as np
 # from geometry_msgs.msg import Pose, Point, Quaternion
 # from trident_msgs.action import GotoPose, HoldPose
 from trident_msgs.msg import MotorOutputs, MotorOutput
@@ -52,7 +53,8 @@ class MotorDriverBase(Node, metaclass=ABCMeta):
 
         # Watchdog timer for the motor output silence (we want the agent to stop if no command is received within a defined timer period)
         self._motor_output_silence_watchdog_timer = self.create_timer(self._motor_output_silence_period, self._motor_output_silence_watchdog_timer_callback)
-        self._motor_output_silence_watchdog_timer.cancel() # The timer should be started in the motor outpout message callback
+        # Timestamp that tracks when the last motor output was actively sent to the motors
+        self._last_motor_output_timestamp = -np.inf
 
         # Subscriptions
         # -------------
@@ -100,8 +102,8 @@ class MotorDriverBase(Node, metaclass=ABCMeta):
         """Converts motor_outputs to Setpoints and sends the values
         to the simulation topic.
         """
-        # Reset the motor output silence watchdog timer.
-        self._motor_output_silence_watchdog_timer.reset()
+        # Motor values are sent to the motors set the last output timestamp.
+        self._last_motor_output_timestamp = self.get_clock().now().nanoseconds/1000000000
         # Create the thruster Setpoints message
         msg = Setpoints()
         outputs = []
@@ -185,16 +187,23 @@ class MotorDriverBase(Node, metaclass=ABCMeta):
         to avoid the agent from escaping form us in case the motor control node stops functioning. 
         """
 
-        self.get_logger().info('Watchdog timer for motor output silence triggered.')
-        # Cancel the timer so it behaves like a oneshot timer.
-        self._motor_output_silence_watchdog_timer.cancel()
-        # Send the output 3 times to increase the chances of the message not being timed out when added to serial write queue.
-        # TODO: Change the motor output message contain a timestamp and let the serial check if the message is too old, and if it 
-        # is, send zero values instead.
-        for _ in range(1):
-            # self.set_zero_motor_output()
-            # self.get_logger().info('Watchdog timer for motor output silence triggered.')
+        self.get_logger().debug('Watchdog timer for motor output silence triggered.')
+        timestamp = self.get_clock().now().nanoseonds/1000000000
+        time_delta = timestamp - self._last_motor_output_timestamp
+        if time_delta > self._motor_output_silence_period:
+            self.set_zero_motor_output()
+            self.get_logger().info('Watchdog timer noticed that no motor output has been sent to to motors within accepted interval. Setting motors to zero.')
             self._motor_driver_state = MotorDriverState.MOTOR_OUTPUT_SILENCE
+
+        # # Cancel the timer so it behaves like a oneshot timer.
+        # # self._motor_output_silence_watchdog_timer.cancel()
+        # # Send the output 3 times to increase the chances of the message not being timed out when added to serial write queue.
+        # # TODO: Change the motor output message contain a timestamp and let the serial check if the message is too old, and if it 
+        # # is, send zero values instead.
+        # for _ in range(1):
+        #     # self.set_zero_motor_output()
+        #     # self.get_logger().info('Watchdog timer for motor output silence triggered.')
+        #     self._motor_driver_state = MotorDriverState.MOTOR_OUTPUT_SILENCE
 
     def _motor_output_sub_callback(self, msg):
         """Callback for messages received on the motor_control/motor_output topic.
@@ -203,16 +212,18 @@ class MotorDriverBase(Node, metaclass=ABCMeta):
         if no motor output message is received within a 100 ms period.
         """
         # Stop the timer so that it doesn't trigger while we are processing the message
-        self._motor_output_silence_watchdog_timer.cancel()
-        # Check if the motors are supposed to be killed
+        # self._motor_output_silence_watchdog_timer.cancel()
+
+        # eck if the motors are supposed to be killed
         if not self.motors_killed:
             motor_outputs = msg.motor_outputs
             # self.get_logger().info(f'Publishing motor output values to the motors. Motor values: {motor_outputs}')
             self._motor_driver_state = MotorDriverState.ACTIVE
             # for motor_num, value in motor_outputs:
             self.__send_motor_outputs(motor_outputs)
-            # Motor values are sent to the motors, reset the watchdog timer
-            self._motor_output_silence_watchdog_timer.reset()
+            # Motor values are sent to the motors set the last output timestamp.
+            self._last_motor_output_timestamp = self.get_clock().now().nanoseconds/1000000000
+            # self._motor_output_silence_watchdog_timer.reset()
         else:
             self.get_logger().info('Motors are currently killed. No motor output sent to the motors.\n' \
                                    'Activate the motors again by calling the motor_driver/motors/activate service.')
