7e65b8e5fefef0d9c132b931b5fdcf671caac33d
Felix Sjoqvist <fst17001@student.mdh.se>
2021-12-07 18:33:26 +0100

    add script for all integration tests

--- trident_software/testing/integration_tests/start_all.sh
+++ trident_software/testing/integration_tests/start_all.sh
@@ -7,6 +7,17 @@ ros1_install=/opt/ros/noetic/setup.bash
 ros2_local_install="install/local_setup.bash"
 ros1_local_install="devel/setup.bash"
 
+session="Trident"
+
+#Color codes that can be used in printf.
+#Remember to finish printf with ${end} to swap back to normal color
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
 
 
 shopt -s expand_aliases
@@ -23,57 +34,141 @@ athena_enable_teleop() { ros2 service call /athena/motor_control/manual_override
 athena_disable_teleop() { ros2 service call /athena/motor_control/manual_override std_srvs/srv/SetBool '{data: false}'; }
 naiad_start_teleop() { ros2 launch naiad_bringup teleop.launch.py; }
 athena_start_teleop() { ros2 launch athena_bringup teleop.launch.py; }
-set +a # Stop exporting
+restart_bridge_sim() {
+    # Create new tmux session
+    tmux kill-session -t $session
+    tmux new-session -d -s $session
 
-# # Set up quality of life aliases
-# aliases=(
-#     "alias start_bridge=\"ros2 run ros1_bridge dynamic_bridge --bridge-all-topics\""
-#     "alias start_sim=\"roslaunch trident_sim trident_simulation.launch camera_show:=false\""
-#     "alias start_athena_core=\"ros2 launch athena_bringup sim.system.launch.py\""
-#     "alias start_naiad_core=\"ros2 launch naiad_bringup sim.system.launch.py\""
-#     "alias start_athena_mission=\"ros2 action send_goal /athena/mission_control/mission/start trident_msgs/action/StartMission \"{}\"\""
-#     "alias start_naiad_mission=\"ros2 action send_goal /naiad/mission_control/mission/start trident_msgs/action/StartMission \"{}\"\""
-#     "alias start_naiad_teleop=\"ros2 launch naiad_bringup teleop.launch.py\""
-#     "alias start_athena_teleop=\"ros2 launch athena_bringup teleop.launch.py\""
-#     "alias enable_naiad_teleop=\"ros2 service call /naiad/motor_control/manual_override std_srvs/srv/SetBool '{data: True}'\""
-#     "alias disable_naiad_teleop=\"ros2 service call /naiad/motor_control/manual_override std_srvs/srv/SetBool '{data: False}'\""
-#     "alias enable_athena_teleop=\"ros2 service call /athena/motor_control/manual_override std_srvs/srv/SetBool '{data: True}'\""
-#     "alias disable_athena_teleop=\"ros2 service call /athena/motor_control/manual_override std_srvs/srv/SetBool '{data: False}'\""
-#     # Add aditional aliases here ...
-# )
-# # Create the actual command for aliasing all the aliases
-# alias_command=${aliases[0]}
-# for ((i = 1; i < ${#aliases[@]}-1; i++)) do
-#     alias_command="${alias_command} && ${aliases[$i]}"
-# done
-
-# Create new tmux session
-session="Trident"
-tmux new-session -d -s $session
-
-# Start the bridge
-tmux new-window -t $session:0 -n 'Ros1bridge'
-tmux send-keys -t $session:0 "source ${ros2_install} &&
+    # Start the bridge
+    tmux new-window -t $session:0 -n 'Ros1bridge'
+    tmux send-keys -t $session:0 "source ${ros2_install} &&
 	source ${ros1_install} &&
 	source ${trident_dir_path}/${ros2_local_install} &&
 	ros2 run ros1_bridge dynamic_bridge --bridge-all-topics" C-m  
 
-# Start the simulator
-tmux new-window -t $session:1 -n 'StonefishSimulator'
-tmux send-keys -t $session:1 "source ${ros1_install} &&
-	source ${simulator_ws_path}/${ros1_local_install} &&
-	roslaunch trident_sim trident_simulation.launch camera_show:=false" C-m
+    # Start the simulator
+    tmux new-window -t $session:1 -n 'StonefishSimulator'
+    if   [ $1 == "athena" ]; then
+	tmux send-keys -t $session:1 "source ${ros1_install} &&
+	    source ${simulator_ws_path}/${ros1_local_install} &&
+	    roslaunch trident_sim athena_simulation.launch" C-m
+    elif [ $1 == "naiad" ]; then
+	tmux send-keys -t $session:1 "source ${ros1_install} &&
+	    source ${simulator_ws_path}/${ros1_local_install} &&
+	    roslaunch trident_sim naiad_simulation.launch camera_show:=false" C-m
+    else
+	tmux send-keys -t $session:1 "source ${ros1_install} &&
+	    source ${simulator_ws_path}/${ros1_local_install} &&
+	    roslaunch trident_sim trident_simulation.launch camera_show:=false" C-m
+    fi
 
-# Start athena core (for simulation)
-tmux new-window -t $session:2 -n 'ATHENACore'
-tmux send-keys -t $session:2 "source ${ros2_install} &&
-	source ${trident_dir_path}/${ros2_local_install} &&
-	ros2 launch athena_bringup sim.system.launch.py" C-m
+    tmux attach-session -t $session
+}
+set +a # Stop exporting
 
-# Start athena core (for simulation)
-tmux new-window -t $session:3 -n 'NAIADCore'
-tmux send-keys -t $session:3 "source ${ros2_install} &&
-	source ${trident_dir_path}/${ros2_local_install} &&
-	ros2 launch naiad_bringup sim.system.launch.py" C-m
 
-tmux attach-session -t $session
+#Source ROS2 environment
+printf "${cyn}\n=========================\nSourcing ROS2 environment\n=========================\n${end}"
+if [ -f "~/ros2_foxy/ros2-linux/setup.bash" ]; then
+    source ~/ros2_foxy/ros2-linux/setup.bash
+elif [ -f "/opt/ros/foxy/setup.bash" ]; then
+    source /opt/ros/foxy/setup.bash
+else
+    printf "${red}WARNING: ROS setup file not found in ~/ros2_foxy/ros2-linux/setup.bash or /opt/ros/foxy/setup.bash.\n${end}"
+fi
+
+printf "${cyn}\n=========================\nBuilding Trident project\n==========================\n${end}"
+cd ../../
+colcon build --symlink-install --packages-skip ros1_bridge
+
+printf "${cyn}\n=========================\nSourcing Trident environment\n======================\n${end}"
+source install/setup.bash
+
+printf "${cyn}\n=========================\nStart integration tests\n===========================\n${end}"
+cd testing/integration_tests
+
+printf "${cyn}\nStarting Ros Bridge and Athena Simulation\n${cyn}"
+#Startup the ros1_bridge and the athena simulation
+restart_bridge_sim "athena"
+
+################################################################
+#Athena test cases
+printf "${cyn}\nStart Athena abort mission test...\n${end}"
+launch_test athena/abort_executing_mission.launch.py &> logs.txt
+if [ $? -ne 0 ]
+then
+    res_athena_abort_mission="${red}Fail${end}"
+else
+    res_athena_abort_mission="${grn}Pass${end}"
+fi
+
+printf "${cyn}\nStart Athena manual override test...\n${end}"
+launch_test athena/manual_override.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_athena_manual_override="${red}Fail${end}"
+else
+    res_athena_manual_override="${grn}Pass${end}"
+fi
+
+printf "${cyn}\nStart Athena simple mission test...\n${end}"
+launch_test athena/simple_mission.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_athena_simple_mission="${red}Fail${end}"
+else
+    res_athena_simple_mission="${grn}Pass${end}"
+fi
+
+printf "${cyn}\nStarting Ros Bridge and Naiad Simulation\n${cyn}"
+#Startup the ros1_bridge and the naiad simulation
+restart_bridge_sim "naiad"
+
+################################################################
+#Naiad test cases
+printf "${cyn}\nStart Naiad abort mission test...\n${end}"
+launch_test naiad/abort_executing_mission.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_naiad_abort_mission="${red}Fail${end}"
+else
+    res_naiad_abort_mission="${grn}Pass${end}"
+fi
+
+printf "${cyn}\nStart Naiad manual override test...\n${end}"
+launch_test naiad/manual_override.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_naiad_manual_override="${red}Fail${end}"
+else
+    res_naiad_manual_override="${grn}Pass${end}"
+fi
+
+printf "${cyn}\nStart Naiad simple mission test...\n${end}"
+launch_test naiad/simple_mission.launch.py &>> logs.txt
+if [ $? -ne 0 ]
+then
+    res_naiad_simple_mission="${red}Fail${end}"
+else
+    res_naiad_simple_mission="${grn}Pass${end}"
+fi
+
+# kill the bridge and simulation
+tmux kill-session -t $session
+
+#Print test results
+printf "\n+---------------------------------------+-----------+\n"
+printf "| Test results: Athena                  |   Status  |\n"
+printf "+---------------------------------------+-----------+\n"
+printf "| Abort Mission                         |   %s    |\n" $res_athena_abort_mission
+printf "| Manual Override                       |   %s    |\n" $res_athena_manual_override
+printf "| Simple Mission                        |   %s    |\n" $res_athena_simple_mission
+printf "+---------------------------------------+-----------+\n\n"
+
+printf "+---------------------------------------+-----------+\n"
+printf "| Test results: Naiad                   |   Status  |\n"
+printf "+---------------------------------------+-----------+\n"
+printf "| Abort Mission                         |   %s    |\n" $res_naiad_abort_mission
+printf "| Manual Override                       |   %s    |\n" $res_naiad_manual_override
+printf "| Simple Mission                        |   %s    |\n" $res_naiad_simple_mission
+printf "+---------------------------------------+-----------+\n\n"
