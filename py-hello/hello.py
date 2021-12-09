8d43b96bb1ab9463b8d48e12021fb318b6271e26
LisaSvens <lisa.svens@telia.com>
2021-12-09 17:53:13 +0100

    backup

--- py-hello/hello.py
+++ py-hello/hello.py
@@ -4,17 +4,7 @@ import cv2
 import os
 import math
 
-from numpy.lib.function_base import median
-# from numpy.lib.function_base import angle
-
-
-# x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
-# plt.plot(x, np.sin(x))       # Plot the sine of each x point
-# plt.show()                   # Display the plot
-
-# im = cv2.imread('seafloor.bmp')
-# cv2.imshow('image', im)
-# cv2.waitKey(0)
+from numpy.lib.function_base import median, mean
 
 
 def plot_quiver(ax, flow, colourmap, margin=0, **kwargs):
@@ -53,24 +43,85 @@ def printImages(frame, hsv, hor, ver, mag, ang):
     col3 = np.concatenate((cv2.cvtColor(mag, cv2.COLOR_GRAY2BGR), cv2.cvtColor(ang, cv2.COLOR_GRAY2BGR)), axis=0)
     return np.concatenate((col1, col2, col3), axis=1)
 
+
+def getLine(img):
+    # print('Inside getLine')
+    # ----------- MORPHOLOGY FOR CENTER LOCALISATION ---------------------------
+    im = (img > 0).astype(np.float32)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(30,30))
+    closing = cv2.morphologyEx(im, cv2.MORPH_CLOSE, kernel)
+    # cv2.imshow('morphology', np.concatenate((im, closing), axis=1))
+
+    # ------------- FIND LINE ------------------------------------------------
+    edges = cv2.Canny(closing.astype(np.uint8),50,150,apertureSize = 7)
+    lines = cv2.HoughLines(edges,1,np.pi/180, 30)
+    # cv2.imshow('edges', norm(edges))
+    if lines is not None:
+        r = lines[0][0][0]
+        theta = lines[0][0][1]
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*r
+        y0 = b*r
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
+        
+        cv2.line(closing,(x1,y1), (x2,y2), .5, 2)
+
+        print('Lines were found')
+        # cv2.imshow('lines found', closing)
+        return [[x1, y1], [x2, y2]]
+    else:
+        print('No lines were found')
+        return None
+    
+
+def getIntersection(line1, line2):
+    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+
+    def det(a, b):
+        return a[0] * b[1] - a[1] * b[0]
+
+    div = det(xdiff, ydiff)
+    if div == 0:
+        raise Exception('lines do not intersect')
+
+    d = (det(*line1), det(*line2))
+    x = int(det(d, xdiff) / div)
+    y = int(det(d, ydiff) / div)
+    return x, y
+
+
 def getAngle(angle, magnitude, center, n):
     # n is the root of the number of samples that will be taken for the rotation calculation
     # rotation is given in degrees
     (r,c) = angle.shape
-    scale = 1
     rotations = []
-    for init_r in range(int(r/(n*2)), int(r-r/(n*2)), int(r/n)): 
-        print('init_r = ' + str(init_r))
-        for init_c in range(int(c/(n*2)), int(c-c/(n*2)), int(c/n)): 
-            print('init_c = ' + str(init_c))
-            goal_r = init_r + scale * magnitude[init_r, init_c] * math.cos(angle[init_r, init_c])
-            print('goal_r = ' + str(goal_r))
-            goal_c = init_c + scale * magnitude[init_r, init_c] * math.sin(angle[init_r, init_c])
-            print('goal_c = ' + str(goal_c))
-            alpha = math.atan2(center[0]-goal_r, center[1]-goal_c)
-            beta = math.atan2(center[0]-init_r, center[1]-init_c)
+    nr = 0
+    nc = 0
+    print(r)
+    print(c)
+    for init_r in range(int(r/(n*2)), int(r-r/(n*2)+1), int(r/n)):      # +1 for rounding errors
+        # print('init_r = ' + str(init_r))
+        nr += 1
+        print(init_r)
+        for init_c in range(int(c/(n*2)), int(c-c/(n*2)+1), int(c/n)): 
+            nc += 1
+            # print('init_c = ' + str(init_c))
+            # print('sample angle = ' + str(angle[init_r, init_c]*360))
+            goal_r = init_r + magnitude[init_r, init_c] * math.cos(angle[init_r, init_c]*2*np.pi)       # (angle*360) * (2*pi/360)
+            # print('goal_r = ' + str(goal_r))
+            goal_c = init_c + magnitude[init_r, init_c] * math.sin(angle[init_r, init_c]*2*np.pi)
+            # print('goal_c = ' + str(goal_c))
+            alpha = math.atan2(center[0]-goal_r, center[1]-goal_c) * 180 / np.pi
+            beta = math.atan2(center[0]-init_r, center[1]-init_c) * 180 / np.pi
             rotations.append(beta - alpha)
-    return median(rotations)*180/(np.pi)
+    print('rotations = ' + str(rotations))
+    print('nr = ' + str(nr) + ', nc = ' + str(nc))
+    return mean(rotations)
 
 
 # ------------ VIDEO CAP START ----------------------------------------
@@ -101,31 +152,17 @@ while(cap.isOpened()):
     # cv2.normalize(magn, magn)
     # horz = norm(horz)
     # vert = norm(vert)
-    cv2.normalize(magn, magn, 0, 1, cv2.NORM_MINMAX)
-    ang = norm(ang)
-    hsv[:,:,0] = ang*255
-    hsv[:,:,2] = magn*2
+    magnNorm = np.empty(magn.shape)
+    cv2.normalize(magn, magnNorm, 0, 1, cv2.NORM_MINMAX)
+    # ang = norm(ang)
+    ang /= 360
+    hsv[:,:,0] = norm(ang)*255
+    hsv[:,:,2] = magnNorm*2
     hsv = cv2.merge((hsv[:,:,0], hsv[:,:,1], hsv[:,:,2]))
     hsv = hsv.astype(np.uint8)
     hsv = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-
-    # ---------- MIN / MAX ----------------------------------------
-    # print('horz = ' + str(np.amax(horz)) + '/' + str(np.amin(horz)))
-    # print('vert = ' + str(np.amax(vert)) + '/' + str(np.amin(vert)))
-    # print('magn = ' + str(np.amax(magn)) + '/' + str(np.amin(magn)))
-    # print('ang = ' + str(np.amax(ang)) + '/' + str(np.amin(ang)))
     
-    # ------------- PLOTTING -----------------------------------------
-    # cv2.imshow('frame', frame)
-    # cv2.imshow('horizontal', horz)
-    # cv2.imshow('vertical', vert)
-    # cv2.imshow('magnitude', magn)
-    # cv2.imshow('angle', ang)
-    # cv2.imshow('hsv', hsv)
-
-    # ------- NEW PLOTTING -----------------------------------------
-    # mat = (horz > 0).astype(np.float32)
-    # print(mat.dtype)
+    # ------- PLOTTING -------------------------------------------
     # cv2.imshow("result", printImages(frame, hsv, horz, vert, magn, ang))
     # cv2.waitKey(0)
 
@@ -165,68 +202,35 @@ while(cap.isOpened()):
     # plt.show()
     # cv2.waitKey(0)
 
-    def getLine(img):
-        # ----------- MORPHOLOGY FOR CENTER LOCALISATION ---------------------------
-        im = (img > 0).astype(np.float32)
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(30,30))
-        closing = cv2.morphologyEx(im, cv2.MORPH_CLOSE, kernel)
-        cv2.imshow('morphology', np.concatenate((im, closing), axis=1))
-        # cv2.waitKey(0)
+    # ----------- CALCULATING ROTATION WITHOUT QUIVER ---------------------------------
 
-        # ------------- FIND LINE ------------------------------------------------
-        edges = cv2.Canny(closing.astype(np.uint8),50,150,apertureSize = 7)
-        lines = cv2.HoughLines(edges,1,np.pi/180, 30)
-        cv2.imshow('edges', norm(edges))
-
-        for r,theta in lines[0]:
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a*r
-            y0 = b*r
-            x1 = int(x0 + 1000*(-b))
-            y1 = int(y0 + 1000*(a))
-            x2 = int(x0 - 1000*(-b))
-            y2 = int(y0 - 1000*(a))
-            
-            cv2.line(closing,(x1,y1), (x2,y2), .5, 2)
-
-            return [[x1, y1], [x2, y2]]
-
-        cv2.imshow('lines found', closing)
-        cv2.waitKey(0)
-
-    def getIntersection(line1, line2):
-        xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
-        ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
-
-        def det(a, b):
-            return a[0] * b[1] - a[1] * b[0]
-
-        div = det(xdiff, ydiff)
-        if div == 0:
-            raise Exception('lines do not intersect')
-
-        d = (det(*line1), det(*line2))
-        x = int(det(d, xdiff) / div)
-        y = int(det(d, ydiff) / div)
-        return x, y
 
     horzLine = getLine(horz)
-    print('1')
     vertLine = getLine(vert)
-    print('2')
-    intersection = getIntersection(horzLine, vertLine)
-    print(intersection)
-    cv2.circle(ang, intersection, radius=20, color=(0, 0, 255), thickness=-1)
-    cv2.imshow('rotation center', ang)
-    cv2.waitKey(0)
     
 
-    # -------------- ROTATION CALCULATION ----------------------------------------
-    print('starting getAngle')
-    n = 3
-    rot = getAngle(ang, magn, intersection, n)
+    # if lines could be found in both horizontal and vertical images
+    if (horzLine and vertLine) is not None:
+        intersection = getIntersection(horzLine, vertLine)
+        cv2.circle(frame, intersection, radius=20, color=(255, 0, 255), thickness=-1)
+        
+        n = 3
+        rot = getAngle(ang, magn, intersection, n)
+    else:
+        ang = np.zeros(ang.shape, ang.dtype)
+        rot = 0.0
+    
     print('rotation angle = ' + str(rot))
+    
+    cv2.imshow('video', frame)
+    cv2.imshow('angle', ang)
+    cv2.imshow('magnitude', norm(magn))
+    # cv2.imshow("result", printImages(frame, hsv, horz, vert, magn, ang))
+    cv2.waitKey(15)
+
+    if abs(rot) > 0.0005:
+        cv2.waitKey(0)
+    
 
     # if cv2.waitKey(1) & 0xFF == ord('q'):
     #     break
@@ -236,16 +240,5 @@ while(cap.isOpened()):
 cap.release()
 cv2.destroyAllWindows()
 
-
-# gt = pd.read_csv('poses/' + seq + '.txt', sep=" ", header=None)
-
-
-U = np.cos(X)
-V = np.sin(Y)
-
-fig1, ax1 = plt.subplots()
-ax1.set_title('Arrows scale with plot width, not view')
-Q = ax1.quiver(X, Y, U, V, units='width')
-# qk = ax1.quiverkey(Q, 0.9, 0.9, 2, r'$2 \frac{m}{s}$', labelpos='E', coordinates='figure')
 
 plt.show()
